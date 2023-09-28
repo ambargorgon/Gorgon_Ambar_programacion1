457a01e5d6d957cea16338b212ee192eca3530df
Ambar Gorgon <ambargorgon@gmail.com>
2023-09-28 10:40:53 -0300

    tp1 commit

---
+++ tp1-Gorgon_Ambar_Comision A.py
@@ -0,0 +1,85 @@
+#Ejercicio 1
+numero1 = 3403
+print("Numero 1: ", numero1)
+
+#Ejercicio 2
+numero2 = 3432.3
+print("Numero2: ", numero2)
+
+#Ejercicio 3
+suma = numero1 + numero2
+print("Suma: ", suma)
+
+#Ejercicio 4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print("Resta: " ,resta)
+print("Multiplicacion: " ,multiplicacion)
+print("Division: " ,division)
+
+#Ejercicio 5
+nombre = "Ambar Gorgon"
+print("nombre: ",nombre)
+
+#Ejercicio 6
+precio = 340.50
+print("precio: $",precio)
+
+#Ejercicio 7 
+descuento = 0.20
+print("descuento: ", descuento)
+
+#Ejercicio 8
+precio_final =  precio - (precio * descuento)
+print("precio final: $", precio_final)
+
+#Ejercicio 9
+cadena = "Probando"
+print("Cadena: ", cadena)
+
+#Ejercicio 10
+longitud = len(cadena)
+print("Longitud: ", longitud)
+
+#Ejercicio 11
+precio = 400.50
+precio_int = int(400.50)
+print("Precio Float: ", precio)
+print("Precio Int: ", precio_int)
+
+#Ejercicio 12
+nombre = "Ambar"
+apellido = "Gorgon"
+nombre_completo = nombre+" "+apellido
+print("Nombre : "+nombre)
+print("Apellido: "+apellido)
+print("Nombre completo: "+nombre_completo)
+
+#Ejercicio 13
+edad = 19
+edad_incrementada = edad-5
+edad_disminuida = edad_incrementada-10
+print("Edad : ", edad)
+print("Edad incrementada en 5: ", edad_incrementada)
+print("Edad disminuida en 10: ", edad_disminuida)
+
+#Ejercicio 14
+altura = 1.60
+altura_multiplicada = altura*4
+altura_dividida = altura_multiplicada/3
+print("Altura : ", altura)
+print("Altura mult en 4: ", altura_multiplicada)
+print("Altura dividida en 3: ", altura_dividida)
+
+#Ejercicio 15
+nombre_mayuscula = "AMBAR"
+nombre_minuscula = nombre_mayuscula.lower()
+print("Nombre mayus : ",nombre_mayuscula)
+print("Nombre minus: ",nombre_minuscula)
+
+#Ejercicio 16
+nombre_inicial_mayuscula = nombre_mayuscula[0].lower() + nombre_mayuscula[1:]
+print("Nombre inicial mayuscula: ", nombre_inicial_mayuscula)
+
+
