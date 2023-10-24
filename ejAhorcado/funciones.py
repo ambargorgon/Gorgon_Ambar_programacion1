3b638de299c14fee6a0b3db8fe198144b06df5ff
Ambar Gorgon <ambargorgon@gmail.com>
2023-10-24 09:48:28 -0300

    fix commit

--- ejAhorcado/funciones.py
+++ ejAhorcado/funciones.py
@@ -1,3 +1,4 @@
+#funcion para descubrir nueva letra
 def new_guess(guess, letter, chosen_word):
     for i in range(0, len(guess)):
         if letter == chosen_word[i]:
--- ejAhorcado/main.py
+++ ejAhorcado/main.py
@@ -3,7 +3,6 @@ from funciones import *
 
 words = ["break", "continue", "while", "switch", "print"]
 chosen_word = random.choice(words)
-print("Palabra", chosen_word)
 
 # imprimir espacio incial
 guess = ["_"]*len(chosen_word)
@@ -15,12 +14,15 @@ while tries > 0:
     letter = input("Ingresa letra: ")
     # imprimir situacion actual
     if letter in chosen_word:
+        #letra adivinada
         guess = new_guess(guess, letter, chosen_word)
     else:
+        #letra no adivinada
         tries -= 1 
         print(f"Incorrecto, te quedan {tries} intentos")
           
     print(" ".join(guess))
+    #palabra completa adivinada
     if letter == chosen_word or chosen_word == ''.join(guess):
         break
 
